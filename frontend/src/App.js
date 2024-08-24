405aadfff5b144b4839c18154bf6fc82ef9fe373
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-24 12:35:04 +0530

    calander

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from 'react-google-login' 
 
 function App() {
+  const responseGoogleSuccess = (response) => {
+    console.log('Login Success:', response);
+  };
+
+  const responseGoogleFailure = (error) => {
+    console.error('Login Failed:', error);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Calendar API</h1>
+      <GoogleLogin
+        clientId='906172210322-1phsel45p4d1jb3odvsme0v0uajupn1n.apps.googleusercontent.com'
+        buttonText='Sign in & Authorize Calendar'
+        onSuccess={responseGoogleSuccess}
+        onFailure={responseGoogleFailure}
+        responseType="code"
+        cookiePolicy={'single_host_origin'}
+        accessType='offline'
+        scope=' openid email profile https://www.googleapis.com/auth/calendar'
+      />
     </div>
   );
 }
